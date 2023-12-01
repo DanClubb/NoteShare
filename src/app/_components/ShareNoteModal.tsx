c6697bfaade1e01d1fb4f4a3a3d19813c106a238
daniel-clubb <danclubb004@gmail.com>
2023-12-01 21:38:08 +0000

    added search filter and scroll lock to share modal

--- src/app/_components/ShareNoteModal.tsx
+++ src/app/_components/ShareNoteModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { api } from "~/trpc/react";
 import User from '../../types/User';
@@ -11,20 +11,29 @@ interface ShareNoteModalProps {
     setShowShareNoteModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-// const filterUsers = (users: User[], currentInput: string): User[] | string => {
-//     users = []
-// if(users.length > 0) return users
-// else return "Can't find user!"
-// }
+const filterUsers = (users: User[], currentInput: string): User[] => {
+   const filteredUsers = users.filter((user) => {
+       return user.email.toLowerCase().includes(currentInput.toLowerCase())
+   })
+if(filterUsers.length > 0) return filteredUsers
+else return []
+}
 
 export default function ShareNoteModal({users, noteId, setShowShareNoteModal}: ShareNoteModalProps) {
     const [email, setEmail] = useState('')
 
     const shareNote = api.note.share.useMutation()
+
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "scroll"
+        };
+    }, []);
     
     return (
         <div className="absolute top-0 left-0 z-10 w-full h-full">
-                <form className="flex flex-col items-center mx-auto p-4 w-4/12 h-2/6 absolute top-2/4 left-2/4 -translate-y-1/2 -translate-x-1/2 z-20 bg-slate-200" onSubmit={(e) => {
+                <form className="flex flex-col items-center mx-auto p-4 w-11/12 sm:w-4/12 h-96 absolute top-2/4 left-2/4 -translate-y-1/2 -translate-x-1/2 z-20 bg-slate-200" onSubmit={(e) => {
                     e.preventDefault()
                     shareNote.mutate({id: noteId, email: email})
                 }
@@ -34,18 +43,21 @@ export default function ShareNoteModal({users, noteId, setShowShareNoteModal}: S
                     >
                         <IoClose size='2rem' />
                     </button>
-                    <div className="w-full">
+                    <div className="flex flex-col w-full">
                         <label>Who would you like to share with?</label>
-                        <input className="mt-2 py-2 px-4 w-full rounded-sm shadow-slate-600 outline-slate-600" value={email} onChange={(e) => setEmail(e.target.value)} />
-                        <div className={`${(email === users[0]?.email || email.length === 0) && 'invisible'} bg-slate-100 rounded-sm border-2 border-slate-300 cursor-pointer`}>
-                            {users.map((user) => (
-                            <p key={user.email} className="py-2 px-4" onClick={(e) => setEmail(e.currentTarget.textContent!)}>{user.email}</p>
-                        ))}
+                        <input className="mt-2 py-2 px-4 w-full rounded-sm shadow-slate-600 outline-slate-600" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        <div className={`${(email === users[0]?.email || email.length === 0) && 'invisible'} max-h-44 bg-slate-100 rounded-sm border-2 border-slate-300 overflow-auto cursor-pointer`}>
+                            {filterUsers(users, email).length === 0 ? 
+                                <p className="py-2 px-4 text-red-600">User not found</p> :
+                                filterUsers(users, email).map((user) => (
+                                    <p key={user.email} className="py-2 px-4 border-b" onClick={(e) => setEmail(e.currentTarget.textContent!)}>{user.email}</p>
+                                ))
+                            }
                         </div>
                         
                     </div>
                     <div className="flex items-center gap-4 mt-auto mr-auto">
-                        <button type="submit" className="flex justify-center items-center w-16 h-10 rounded-sm border-2 border-slate-600 shadow-lg shadow-cyan-200 hover:shadow hover:shadow-cyan-200 hover:scale-95 hover:transition">
+                        <button type="submit" className="flex justify-center items-center w-16 h-10 rounded-sm border-2 border-slate-600 shadow-lg shadow-cyan-200 hover:shadow hover:shadow-cyan-200 active:scale-95 hover:transition">
                         {shareNote.isLoading ? <LoadingSpinner /> : 'Share'}
                     </button>
                     {shareNote.isSuccess && <p className="text-lime-600">Shared successfully</p>}
